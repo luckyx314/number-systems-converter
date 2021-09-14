f6a3ed5314680a592cc89c77245560e86e747d09
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-09-14 13:52:04 +0800

    First Commit

---
+++ conversions.js
@@ -0,0 +1,39 @@
+
+// decimal conversions 
+function decToBinary(given) {
+    console.log(given/2)
+}
+
+function decToOctal(given) {
+    console.log(given/8)
+}
+
+function decToHex(given) {
+    console.log(given/16)
+}
+
+// binary conversions
+function binaryToDec(given) {
+
+}
+
+function binaryToHex(given) {
+
+}
+
+function binaryToOctal(given) {
+
+}
+
+// octal conversions
+function octalToBinary(given) {
+
+}
+
+function octalToHex(given) {
+
+}
+
+function octalToDec(given) {
+    
+}
