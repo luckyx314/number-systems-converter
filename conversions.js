743afbbd955e60e803675d69b39b2a9693bc04fb
unknown <luckyjonesbuayan@gmail.com>
2021-09-21 22:49:04 +0700

    Modified conversions.js - initialized a hex to deci conversion

--- conversions.js
+++ conversions.js
@@ -1,39 +1,92 @@
+let x = {
+    type: 'hexadecimal',
+    data: "abcd.FA"
+};
 
-// decimal conversions 
-function decToBinary(given) {
-    console.log(given/2)
+
+const hexToDeci = {
+    a: 10,
+    b: 11,
+    c: 12,
+    d: 13,
+    e: 14,
+    f: 15
 }
 
-function decToOctal(given) {
-    console.log(given/8)
+let givenArray = separateMSDandLSD(x);
+
+
+function separateMSDandLSD(given) {
+    const array = [];
+    const lowercasedGiven = given.data.toLowerCase();
+    const MSDpattern = /\w+/i;
+    const LSDpattern = /[.]\w+/i
+
+    const MSD = lowercasedGiven.match(MSDpattern)[0]
+    const LSD = lowercasedGiven.match(LSDpattern)[0].replace('.', '')
+
+    array.push(MSD, LSD)
+
+    return array
 }
 
-function decToHex(given) {
-    console.log(given/16)
-}
 
-// binary conversions
-function binaryToDec(given) {
+function convertHexToDeci(arr) {
+    const MSD = arr[0].split('');
+    const LSD = arr[1].split('');
 
-}
+    let msdArr = [];
+    let lsdArr = [];
 
-function binaryToHex(given) {
+    MSD.forEach(char => {
+        if (hexToDeci[char]) {
+            msdArr.push(hexToDeci[char])
+        } else {
+            msdArr.push(Number(char))
+        }
+    })
 
-}
 
-function binaryToOctal(given) {
+    LSD.forEach(char => {
+        if (hexToDeci[char]) {
+            lsdArr.push(hexToDeci[char])
+        } else {
+            lsdArr.push(Number(char))
+        }
+    })
 
-}
+    console.log(msdArr)
+    console.log(lsdArr)
 
-// octal conversions
-function octalToBinary(given) {
-
-}
-
-function octalToHex(given) {
-
-}
-
-function octalToDec(given) {
     
 }
+
+convertHexToDeci(givenArray)
+
+
+
+    // lowercasedGiven.split('').forEach(char => {
+    //     if (!hexToDeci[char]) {
+    //         MSD.push(Number(char))
+    //     } else if (hexToDeci[char]) {
+    //         MSD.push(hexToDeci[char])
+    //     }
+    // })
+
+    // let exponents = []
+
+    // for (let i = MSD.length - 1; i >= 0; i--) {
+    //     // console.log('exponent', Math.pow(16, i))
+    //     exponents.push(Math.pow(16, i))
+    // }
+
+    // MSD.push(exponents)
+
+    // let final = [];
+    // for (let i=0; i < MSD.length; i++) {
+    //     for (let j = 0; j < MSD[i].length; j++) {
+    //         final.push(MSD[i][j] * MSD[j])
+    //     }
+    // }
+
+    // console.log(final.reduce((total, value) => total + value))
