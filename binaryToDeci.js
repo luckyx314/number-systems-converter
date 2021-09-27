52378659f438ba3f1bd83d539f3d30350b8f1b63
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-09-27 09:39:13 +0800

    Modified binaryToDeci.js - created an object with the data to be converted

--- binaryToDeci.js
+++ binaryToDeci.js
@@ -1,5 +1,20 @@
 x = {
     type: "binary",
-    data: 101011110011.10101101
+    data: '101011110011.10101101',
+    msdAndLsdArray: function () {
+        const given = this.data;
+        const msdPattern = /\w+/
+        const lsdPattern = /[.]\w+/
+        
+        const MSD = given.match(msdPattern)[0].split('');
+
+        if (given.indexOf('.') > 0) {
+            const LSD = given.match(lsdPattern)[0].replace('.','').split('');
+            return [MSD, LSD]
+        } else {
+            return MSD;
+        }
+    } 
 }
 
+console.log(x.msdAndLsdArray())
