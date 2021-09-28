5bf7336b82799301ac0a56ad9a970efb4fa3a0bd
Lucky Jones Uayan <luckyjonesbuayan@gmail.com>
2021-09-28 21:03:49 +0800

    Can convert from binary-deci & octal-deci

--- src/components/logic/NumberSystemClass.js
+++ src/components/logic/NumberSystemClass.js
@@ -1,7 +1,15 @@
 class NumberSystem {
-    constructor(type, data) {
-        this.type = type;
-        this.data = data;
+    constructor(numberSystem, data) {
+        this.numberSystem = numberSystem;
+        this.data = data.toString();
+    }
+
+    getnumberSystem() {
+        return this.numberSystem;
+    }
+
+    getData() {
+        return this.data;
     }
 
     getMsdAndLsd() {
@@ -19,16 +27,171 @@ class NumberSystem {
                 return MSD;
             }
         } else {
-            return `provide a valid ${this.type} number`
+            return `provide a valid ${this.numberSystem} number`
+        }
+    }
+
+    ifLsd() {
+        if (this.data) {
+            const lsdPattern = /[.]\w+/
+            const given = this.data;
+            return lsdPattern.test(given)
+        }
+    }
+
+    toDecimal(base) {
+            let msd;
+            let lsd;
+            const checkFraction = typeof this.getMsdAndLsd()[1] === 'object';
+
+            if (checkFraction) {
+                msd = this.getMsdAndLsd()[0].map(num => Number(num));
+                lsd = this.getMsdAndLsd()[1].map(num => Number(num));
+            } else if (this.ifLsd()) {
+                lsd = this.getMsdAndLsd().map(num => Number(num));
+            } else {
+                msd = this.getMsdAndLsd().map(num => Number(num));
+            }
+
+            let finalMsd
+            if (msd) {
+                finalMsd = [...msd].map((num, index) => {
+                    return num * Math.pow(base, (Math.abs(index - (msd.length - 1))))
+                }).reduce((total, value) => total + value);
+            }
+
+            let finalLsd;
+            if (checkFraction || this.ifLsd()) {
+                finalLsd = [...lsd].map((num,index) => {
+                    return num * Math.pow(base, -(index + 1))
+                }).reduce((total, value) => total + value);
+
+                if (checkFraction) {
+                    return finalMsd + finalLsd;
+                } else if (this.ifLsd()) {
+                    return finalLsd;
+                }
+            } else {
+                return finalMsd;
+            }
+    }
+}
+
+
+// To decimal
+class BinaryToDecimal extends NumberSystem{
+    constructor(numberSystem, data) {
+        super(numberSystem, data);
+    }
+
+    validate() {
+        const data = this.getData();
+        const pattern = /[2-9|a-z]+/g
+
+        if (!pattern.test(data)) {
+            return true;
+        } else {
+            return false
+        }
+
+    }
+
+    convertToDecimal() {
+        if (this.validate()) {
+            return this.toDecimal(2)
+        } else {
+            return `Not a valid ${this.numberSystem} number.`
         }
     }
 }
 
-class ConvertToDecimal extends NumberSystem{
-    constructor(type, data) {
-        super(type, data);
+
+
+class OctalToDecimal extends NumberSystem{
+    constructor(numberSystem, data) {
+        super(numberSystem, data);
+        
+    }
+
+    validate() {
+        const data = this.getData();
+        const pattern = /[8-9|a-z]+/g
+
+        if (!pattern.test(data)) {
+            return true;
+        } else {
+            return false
+        }
+
+    }
+
+    convertToDecimal() {
+        if (this.validate()) {
+            return this.toDecimal(8)
+        } else {
+            return `Not a valid ${this.numberSystem} number.`
+        }
     }
 }
 
-const binary = new NumberSystem('binary', '10010010.0011');
-console.log(binary.getMsdAndLsd())
+
+
+class HexToDecimal extends NumberSystem {
+    constructor(numberSystem, data) {
+        super(numberSystem, data)
+        this.hexTable = {
+            a: 10,
+            b: 11,
+            c: 12,
+            d: 13,
+            e: 14,
+            f: 15
+        }
+    }
+
+    validate() {
+        const pattern = /[a-z]/g;
+        const data = this.data;
+
+        return pattern.test(data)
+        return data.match(pattern)
+
+    }
+
+    getHexMsd() {
+        const data = this.getData();
+
+
+    }
+    
+    getHexLsd() {
+        const data = this.getData();
+        const pattern = /\w+/g
+
+        console.log(data.match(pattern))
+
+    }
+}
+
+
+// To Octal
+
+// To Binary
+
+// To Hexadecimal
+
+const hex = new HexToDecimal('hexadecimal', '1abf.3c')
+const binary = new BinaryToDecimal('binary', '100110.01010')
+const octal = new OctalToDecimal('octal', '135.62')
+
+// console.log(hex.getHexMsd())
+console.log(hex.getHexLsd())
+// console.log(binary.convertToDecimal())
+// console.log(octal.convertToDecimal())
+
+export {
+    NumberSystem,
+    BinaryToDecimal,
+    HexToDecimal,
+    OctalToDecimal
+}
